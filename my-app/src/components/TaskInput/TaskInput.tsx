5d8a3dc8810ea851698f2894313965016af9043c
huanhutlinh <huanhutlinh128@gmail.com>
2023-02-03 16:39:50 +0700

    Add Tasks and Done Task checkbox

--- my-app/src/components/TaskInput/TaskInput.tsx
+++ my-app/src/components/TaskInput/TaskInput.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import styles from './taskInput.module.scss'
-export default function TaskInput() {
+
+interface TaskInputProps {
+  addTodo: (name: string) => void
+}
+
+export default function TaskInput(props: TaskInputProps) {
+  const { addTodo } = props
+  const [name, setName] = useState<string>('')
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    addTodo(name)
+    setName('')
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    setName(value)
+  }
   return (
     <div className='mb-2'>
       <h1 className={styles.title}>ToDo List typescript</h1>
-      <form action='' className={styles.form}>
-        <input type='text' placeholder='text here' />
+      <form action='' className={styles.form} onSubmit={handleSubmit}>
+        <input type='text' placeholder='text here' value={name} onChange={onChangeInput} />
         <button type='submit'>+</button>
       </form>
     </div>
